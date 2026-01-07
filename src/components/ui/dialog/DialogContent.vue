eaf40270e22be8b881d43594d1c08c963ebbf3b5
Rin <shinohara-rin@users.noreply.github.com>
2026-01-08 07:35:58 +0800

    Revert "chore: acknowledge feedback"
    This reverts commit 21ce1a1e24f1935ccc968fdf4d4038af3f437bbb.

--- src/components/ui/dialog/DialogContent.vue
+++ src/components/ui/dialog/DialogContent.vue
@@ -6,11 +6,11 @@ import { DialogClose, DialogContent, DialogOverlay, DialogPortal, useForwardProp
 import { computed } from 'vue'
 import { cn } from '~/utils'
 
-const props = defineProps<DialogContentProps & { class?: HTMLAttributes['class'] }>()
+const props = defineProps<DialogContentProps & { class?: HTMLAttributes['class'], overlayClass?: HTMLAttributes['class'] }>()
 const emits = defineEmits<DialogContentEmits>()
 
 const delegatedProps = computed(() => {
-  const { class: _, ...delegated } = props
+  const { class: _, overlayClass: __, ...delegated } = props
 
   return delegated
 })
@@ -21,7 +21,12 @@ const forwarded = useForwardPropsEmits(delegatedProps, emits)
 <template>
   <DialogPortal>
     <DialogOverlay
-      class="fixed inset-0 z-50 bg-black/80 data-[state=closed]:animate-out data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0"
+      :class="
+        cn(
+          'fixed inset-0 z-50 bg-black/80 data-[state=closed]:animate-out data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=closed]:fade-out-0',
+          props.overlayClass,
+        )
+      "
     />
     <DialogContent
       v-bind="forwarded"
